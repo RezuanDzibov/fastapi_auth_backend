a48468fd2f9c72d244bc3d78079d32a166e231d1
RezuanDzibov <rezuan.dzbov@gmail.com>
2022-01-31 13:44:44 +0300

    Initial commit

---
+++ config/settings.py
@@ -0,0 +1,21 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+dotenv_path = Path(f"{BASE_DIR}/.env")
+load_dotenv(dotenv_path=dotenv_path)
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+
+SQL_ENGINE = os.environ.get('SQL_ENGINE')
+SQL_USER = os.environ.get('SQL_USER')
+SQL_PASSWORD = os.environ.get('SQL_PASSWORD')
+SQL_HOST = os.environ.get('SQL_HOST')
+SQL_PORT = os.environ.get('SQL_PORT')
+SQL_DATABASE = os.environ.get('SQL_DATABASE')
