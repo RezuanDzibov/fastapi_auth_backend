e959c847eb6e785ac2035ed2c9e6e63f7eada732
RezuanDzibov <rezuan.dzbov@gmail.com>
2022-02-01 16:16:30 +0300

    Create auth app

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import uvicorn
 from fastapi import FastAPI
 
+from routes import routes
 
 app = FastAPI()
+app.include_router(routes)
 
 
 if __name__ == '__main__':
---
+++ routes.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter
+
+from auth.endpoints import auth_router
+
+
+routes = APIRouter()
+
+
+routes.include_router(auth_router, prefix='/auth', tags=['auth'])
