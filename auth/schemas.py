2a21891ebfd6afe89195e2ce965a70f141af1021
RezuanDzibov <rezuan.dzbov@gmail.com>
2022-02-01 23:07:21 +0300

    Add TokenPayload schema

--- auth/schemas.py
+++ auth/schemas.py
@@ -1,6 +1,10 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, UUID4
 
 
 class Token(BaseModel):
     access_token: str
     token_type: str
+
+
+class TokenPayload(BaseModel):
+    user_id: UUID4 = None
